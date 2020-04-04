ace5f01a7ddfa0e93ac903c7b86baac1119768a0
djahiz <djahiz.meliani@gmail.com>
2020-04-04 20:01:18 +0200

    del file & update

--- fonctions_calib.py
+++ fonctions_calib.py
@@ -41,55 +41,19 @@ def vol_impli_sabr_lognormale(alpha,beta,rho,nu,forward,strike,expiry):
 
     return vol
 
-# fonction de calcul de la vol impli avec l'approximation normale
-# prend en parametre les grandeurs calibrés ainsi qu'une valeur de forward, strike et de date d'expiration
-def vol_impli_sabr_normale(alpha,beta,rho,nu,forward,strike,expiry):
-
-    if forward == strike: 
-        V = (forward*strike)**((1-beta)/2.)
-        A = 1 + (((-beta)*(2-beta)*alpha**2)/(24.*(V**2))+(alpha*beta*nu*rho)/(4.*V)+((nu**2)*(2-3*(rho**2))/24.))*expiry
-        vol = (alpha*((forward*strike)**(beta/2.))*A)
-    else:
-        V = (forward*strike)**((1-beta)/2.)
-        z = (nu/alpha)*V*math.log(forward/strike)
-        xi = math.log((math.sqrt(1-2*rho*z+z**2)+z-rho)/(1-rho))
-        A = 1 + (((-beta)*(2-beta)*alpha**2)/(24.*(V**2))+(alpha*beta*nu*rho)/(4.*V)+((nu**2)*(2-3*(rho**2))/24.))*expiry
-        B = 1 + (1/24.)*((math.log(forward/strike))**2)+(1/1920.)*((math.log(forward/strike))**4)
-        C = 1 + ((1-beta)**2)*(1/24.)*((math.log(forward/strike))**2)+((1-beta)**4)*(1/1920.)*((math.log(forward/strike))**4)
-        vol = (alpha*((forward*strike)**(beta/2.))*A*B*z)/(xi*C)
-
-    return vol
-
-
 # fonction objectif pour la minimisation
-def objfunc(parametre,forward,strikes,expiry,market_vol,type_approx):
+def objfunc(parametre,forward,strikes,expiry,market_vol):
     sum_error = 0
     for j in range(len(strikes)):
         sum_error += 0  
         if market_vol[j] != 0:  
-            if type_approx == "lognormale":  
-                vol = vol_impli_sabr_lognormale(parametre[0],parametre[1],parametre[2],parametre[3],forward,strikes[j],expiry)
-            elif type_approx == "normale":
-                vol = vol_impli_sabr_normale(parametre[0],parametre[1],parametre[2],parametre[3],forward,strikes[j],expiry)
+            vol = vol_impli_sabr_lognormale(parametre[0],parametre[1],parametre[2],parametre[3],forward,strikes[j],expiry)
             sum_error += (vol - market_vol[j])**2
     return math.sqrt(sum_error)
 
 # calibration des paramètres du modèle
 # la fonction "minimize" de la librairie scipy permets de minimiser l'erreur calculée avec la fonction objectif précédente
 # la méthode "SLSQP" permets de minimiser sous les contraintes données dans le paramètre "bounds" de la fonction "minimize"
-def calibration(valeurs_initiales,forward,strikes,expiry,market_vol,type_approx):
-    limites = ((0.001,None),(0,1),(-0.999,0.999),(0.001,None))
-    res = minimize(objfunc,valeurs_initiales,(forward,strikes,expiry,market_vol,type_approx),bounds = limites, method='SLSQP')
-    return res.x
-
-# calibration des paramètres du modèle sans le beta
-def calibration_beta_fixe(valeurs_initiales,beta_fixe,forward,strikes,expiry,market_vol,type_approx):
-    limites = ((0.001,None),(beta_fixe,beta_fixe),(-0.999,0.999),(0.001,None))
-    res = minimize(objfunc,valeurs_initiales,(forward,strikes,expiry,market_vol,type_approx),bounds = limites, method='SLSQP')
-    return res.x
-
-# calibration des paramètres du modèle sans le rho
-def calibration_rho_fixe(valeurs_initiales,rho_fixe,forward,strikes,expiry,market_vol,type_approx):
-    limites = ((0.001,None),(0,1),(rho_fixe,rho_fixe),(0.001,None))
+def calibration(valeurs_initiales,limites,forward,strikes,expiry,market_vol,type_approx):
     res = minimize(objfunc,valeurs_initiales,(forward,strikes,expiry,market_vol,type_approx),bounds = limites, method='SLSQP')
     return res.x
--- script_calibration.py
+++ script_calibration.py
@@ -1,3 +1,36 @@
+"""
+    Calibration module
+    ======================
+ 
+    Use it to import very obvious functions.
+ 
+    :Example:
+ 
+    >>> from obvious import add
+    >>> add(1, 1)
+    2
+ 
+    This is a subtitle
+    -------------------
+ 
+    You can say so many things here ! You can say so many things here !
+    You can say so many things here ! You can say so many things here !
+    You can say so many things here ! You can say so many things here !
+    You can say so many things here ! You can say so many things here !
+ 
+    This is another subtitle
+    ------------------------
+ 
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+ 
+ 
+"""
+
 from fonctions_calib import *
 import csv
 import matplotlib.pyplot as plt
@@ -12,87 +45,46 @@ import time
 fields_param = ["Tenor","Expiry","Alpha","Beta","Rho","Nu"]
 # liste qui contiendra les ligne du fichier .csv
 param = []
-# Choix approx normale ou lognormale
+
+name_input_file = os.getcwd()+"\\inputs_lognormal.csv"
+
 while True:
-    print("Choix de l'approximation: ")
-    print("[1]: Normale")
-    print("[2]: Lognormale non shifté")
-    print("[3]: Lognormale shifté")
-    try:
-        choix = int(input("Saisissez une valeur : "))
-        if choix == 1 or choix == 2 or choix == 3 or choix == 4:
-            break
-        else:
-            os.system("cls")
-    except:
-        os.system("cls")
-
-if choix == 1:
-    type_approx = "normale"
-    modele_shift = ""
-    name_input_file = os.getcwd()+"\\inputs_normal.csv"
-elif choix == 2:
-    type_approx = "lognormale"
-    modele_shift = ""
-    name_input_file = os.getcwd()+"\\inputs_lognormal.csv"
-else:
-    type_approx = "lognormale"
-    modele_shift = "shift"
-    name_input_file = os.getcwd()+"\\inputs_lognormal_shift.csv"
-
-if modele_shift == "shift":
-    # Choix valeur de shift
-    while True:
-        print("Choisissez une valeur de shift (en bps): ")
-        try:
-            valeur_shift = float(input("Saisissez une valeur : "))
-            break
-        except:
-            os.system("cls")
-
-# Choix calibration avec beta fixé
-while True:
-    print("Fixer la valeur d'un paramètre ?: ")
+    print("Fixe parameter ?: ")
     print("[0]: Aucun")
     print("[1]: Beta")
     print("[2]: Rho")
     try:
-        choix_param = int(input("Saisissez une valeur : "))
-        if choix_param == 0 or choix_param == 1 or choix_param == 2:
+        choice = int(input("Enter a value: "))
+        if choice in [0, 1, 2]:
             break
         else:
             os.system("cls")
-    except:
+    except ValueError:
         os.system("cls")
 
-if choix_param == 1:
-    # Choix valeur de beta
+if choice == 1:
     while True:
-        print("Choisissez une valeur de Beta dans [0;1]: ")
+        print("Enter a value for Beta in [0;1]: ")
         try:
-            beta_fixe = float(input("Saisissez une valeur : "))
+            beta_fixe = float(input("Beta = "))
             if beta_fixe >= 0 and beta_fixe <= 1:
                 break
             else:
                 os.system("cls")
-        except:
+        except ValueError:
             os.system("cls")
 
-if choix_param == 2:
-    # Choix valeur de rho
+if choice == 2:
     while True:
-        print("Choisissez une valeur de Rho dans [-0.99;0.99]: ")
+        print("Enter a value for Rho in [-0.99;0.99]: ")
         try:
-            rho_fixe = float(input("Saisissez une valeur : "))
+            rho_fixe = float(input("Rho = "))
             if rho_fixe >= -0.99 and rho_fixe <= 0.99:
                 break
             else:
                 os.system("cls")
-        except:
+        except ValueError:
             os.system("cls")
-
-# valeurs initiales pour la calibration
-valeurs_initiales = [0.001,0.5,0,0.001]
 
 # lecture des données en inputs (tenor, expiry, forward, market vol par spread)
 with open(name_input_file,"rt") as input_file:
@@ -108,51 +100,43 @@ for i in range(3,len(data[0])):
 # pour chaque ligne on calibre un jeu de paramètres puis on génère les graphs de comparaison
 data.pop(0)
 for row in data:
-    # si on a choisi le modele avec shifte
-    if modele_shift == "shift":
-        forward = float(row[2]) + 0.0001*valeur_shift
-        strikes = [float(row[2])+ 0.0001*(k+valeur_shift) for k in K]
-    else:
-        strikes = [0.0001*k for k in K]
-        forward = float(row[2])  
+    
+    strikes = [0.0001*k for k in K]
+    forward = float(row[2])
     # on recupere la vol market
     vol_market = [float(v) for v in row[3:len(row)]]
     # si on veut calibrer pour une valeur fixe de beta
-    if choix_param == 1:
+
+    if choice == 1:
         valeurs_initiales = [0.001,beta_fixe,0,0.001]
-        result = calibration_beta_fixe(valeurs_initiales,beta_fixe,forward,strikes,float(row[1]),vol_market,type_approx)
+        limites = ((0.001,None),(beta_fixe,beta_fixe),(-0.999,0.999),(0.001,None))
     # si on veut calibrer pour une valeur fixe de rho
-    elif choix_param == 2:
+    elif choice == 2:
         valeurs_initiales = [0.001,0.5,rho_fixe,0.001]
-        result = calibration_rho_fixe(valeurs_initiales,rho_fixe,forward,strikes,float(row[1]),vol_market,type_approx)
+        limites = ((0.001,None),(0,1),(rho_fixe,rho_fixe),(0.001,None))
     # sinon on calibre les 4 parametres
     else:
-        result = calibration(valeurs_initiales,forward,strikes,float(row[1]),vol_market,type_approx)
+        valeurs_initiales = [0.001, 0.5, 0, 0.001]
+        limites = ((0.001,None),(0,1),(rho_fixe,rho_fixe),(0.001,None))
+        
+    result = calibration(valeurs_initiales,forward,strikes,float(row[1]),vol_market,type_approx)
 
     param.append([row[0],row[1],result[0],result[1],result[2],result[3]])
 
-    if(type_approx == "normale"):
-        vol_impli = [100*vol_impli_sabr_normale(result[0],result[1],result[2],result[3],forward,strike,float(row[1])) for strike in strikes]
-        plt.plot(K,vol_impli,label="Hagan normale")
-    else:
-        vol_impli = [100*vol_impli_sabr_lognormale(result[0],result[1],result[2],result[3],forward,strike,float(row[1])) for strike in strikes]
-        plt.plot(K,vol_impli,label="Hagan lognormale "+modele_shift)
+    
+    vol_impli = [100*vol_impli_sabr_lognormale(result[0],result[1],result[2],result[3],forward,strike,float(row[1])) for strike in strikes]
+    plt.plot(K,vol_impli,label="Hagan lognormale ")
 
     # enregistrement des résultats (parametres fitté + graphs) 
     plt.plot(K,[100*v for v in vol_market],label="Marché")
     plt.legend()
     plt.xlabel("Strike (bps)")
     plt.ylabel("Volatilité Implicite (%)")
-    if modele_shift == "shift":
-        name_file_outputs = os.getcwd()+"\\Outputs\\avec_shift\\"+type_approx
-        if not os.path.exists(name_file_outputs):
-            os.makedirs(name_file_outputs)
-        plt.savefig(name_file_outputs+"\\Tenor "+row[0]+"y Expiry "+row[1]+"y_"+type_approx+"_shift.png")
-    else:
-        name_file_outputs = os.getcwd()+"\\Outputs\\sans_shift\\"+type_approx
-        if not os.path.exists(name_file_outputs):
-            os.makedirs(name_file_outputs)
-        plt.savefig(name_file_outputs+"\\Tenor "+row[0]+"y Expiry "+row[1]+"y_"+type_approx+".png")
+    
+    name_file_outputs = os.getcwd()+"\\Outputs"
+    if not os.path.exists(name_file_outputs):
+        os.makedirs(name_file_outputs)
+    plt.savefig(name_file_outputs+"\\Tenor "+row[0]+"y Expiry "+row[1]+"y_.png")
     plt.clf()
 
 # création du fichier csv de sortie et écriture des résultats
